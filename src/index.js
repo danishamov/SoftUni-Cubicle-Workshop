7bb7e455a7c79b83a5785e37205febd1449acf66
danishamov <danishamov@gmail.com>
2023-09-29 23:13:08 +0100

    Create server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello from England");
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hollo from England");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
